02131dee2cf4f84759df79909216981f499745bb
Gahee <jghgahee12@gmail.com>
2021-01-14 18:08:07 +0900

    회원가입을 위한 router 작성함

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // index.js는 백엔드의 시작점
+// 서버 만들기
 const express = require('express')  // express module을 가져옴
 const app = express()
 const port = 5000
+
+const bodyParser = require('body-parser');
+const{ User } = require("./models/User");
+
+//application/x-www-form-urlencoded처럼 생긴 데이터를 분석해서 가져올 수 있게 함
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json타입으로 된 데이터를 분석해서 가져올 수 있게 함
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://dbgahee:loh5558@cluster0.oecku.mongodb.net/dbgahee?retryWrites=true&w=majority', {
@@ -12,6 +21,19 @@ mongoose.connect('mongodb+srv://dbgahee:loh5558@cluster0.oecku.mongodb.net/dbgah
 app.get('/', (req, res) => {    
   res.send('Hello World! 하이루')
 })  // /디렉토리(루트 디렉토리)에서 헬로 월드 실행
+
+//회원가입을 위한 router
+app.post('/register',(req,res) => {
+    //회원갑입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어줌
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ succecc: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 // ctrl + c -> 작업 종료, 서버 종료  
 
